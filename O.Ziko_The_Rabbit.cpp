33bc683c2e0e5adf41fdfdbdd5fa16a8d70673b0
abdallahEssam49 <76582213+abdallahEssam49@users.noreply.github.com>
2022-06-11 13:54:45 +0200

    Create O.Ziko_The_Rabbit.cpp

---
+++ O.Ziko_The_Rabbit.cpp
@@ -0,0 +1,44 @@
+#include <bits/stdc++.h>
+
+#define clr(str , charr) memset(str,charr, sizeof(str))
+using namespace  std;
+using namespace std::chrono;
+
+const int mod =  1e9 +5;
+
+#define test int t; cin>>t; while(t--)
+#define print( i , output ) cout << "Case #" << i << ": " << output ;
+
+#define ll long long
+#define pi 3.1415926
+
+
+#define l length()
+#define preci(x) cout << fixed<<setprecision(x)
+#define infmax LONG_LONG_MAX
+#define infmin LONG_LONG_MIN
+#define bit __builtin_popcountll /*It returns the numbers of set bits in an integer 
+(the number of ones in the binary representation of the integer).*/
+
+#define all(v)			(v).begin(),(v).end()
+#define sz(v)			(((int)((v).size())))
+void fast(){
+ ios_base::sync_with_stdio(0);cin.tie(0); cout.tie(0);
+}
+
+
+
+void solve ()
+{
+    int n; cin>>n; cout << n + n-1 << "\n"; 
+}
+
+
+int main() 
+{
+  freopen ("ziko.in","r",stdin);
+  fast();
+  test
+  solve();
+  //calctime();
+}
